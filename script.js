785a432c9556856f45d33a7b6b4eb9d258d04ce4
Justin Legros <65736254+justinlegros@users.noreply.github.com>
2024-06-17 15:10:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const dateTime = document.getElementById("dateTime");
 const dateP = document.getElementById("dateP");
 const timeP = document.getElementById("timeP");
 const submitBtn = document.getElementById("submitBtn")
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "Febuary", "March", "April", "June", "July", "August", "September", "November", "December"];
 
 function updateDateTime() {
   const now = new Date();
@@ -9,9 +11,12 @@ function updateDateTime() {
   const currentDateTime = now.toLocaleString();
   const currentTime = currentDateTime.split(',')[1];
   const currentDate = currentDateTime.split(',')[0];
+  const currentMonth = monthNames[now.getMonth()];
+  const currentDay = dayNames[now.getDay()];
+  const currentDateDay = now.getDate();
   
+  dateP.innerHTML = `${currentDay} ${currentMonth}-${currentDateDay}`;
   timeP.innerHTML = currentTime;
-  dateP.innerHTML = currentDate;
 }
 
 submitBtn.addEventListener("click", () =>

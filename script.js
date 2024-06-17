80b675b46a1105d3c78b57892132330f6c13ec80
Justin Legros <65736254+justinlegros@users.noreply.github.com>
2024-06-17 15:19:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ submitBtn.addEventListener("click", () =>
 );
 
 setInterval(updateDateTime, 1000);
+
+const canWakeLock = () => 'wakeLock' in navigator;
+
+let wakelock;
+async function lockWakeState() {
+  if(!canWakeLock()) return;
+  try {
+    wakelock = await navigator.wakeLock.request();
+    wakelock.addEventListener('release', () => {
+      console.log('Screen Wake State Locked:', !wakelock.released);
+    });
+    console.log('Screen Wake State Locked:', !wakelock.released);
+  } catch(e) {
+    console.error('Failed to lock wake state with reason:', e.message);
+  }
+}
+
